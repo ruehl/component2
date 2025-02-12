1b93f9265620437fcef40c85893e00e278f41e00
Christian Ruehl <ruehl@objectix.de>
2025-02-12 11:35:03 +0000

    fixed bundler build script

--- .bundler/build.sh
+++ .bundler/build.sh
@@ -2,9 +2,9 @@
 # Arg1: path to the output directory to put built artifacts into
 # Arg2: any configuration tag(s) to use during build
 # Arg3: the DS device identifier to build for
-$OUTPUT=$1
-$CONFIG=$2
-$DEVICE=$3
+OUTPUT=$1
+CONFIG=$2
+DEVICE=$3
 
 echo "Building Component No. 2"
 echo "#!/bin/bash" > $OUTPUT/bin.sh
