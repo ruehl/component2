307fb0181310ebb0c5414a50d1c2d920dccb5ed7
Christian Ruehl <ruehl@objectix.de>
2025-02-12 11:31:15 +0000

    extended build script

--- .bundler/build.sh
+++ .bundler/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
+# Arg1: path to the output directory to put built artifacts into
+# Arg2: any configuration tag(s) to use during build
+# Arg3: the DS device identifier to build for
+$OUTPUT=$1
+$CONFIG=$2
+$DEVICE=$3
+
 echo "Building Component No. 2"
-echo "#!/bin/bash" > /output/bin.sh
-echo "echo 'Hello World from Component No. 2'" >> /output/bin.sh
+echo "#!/bin/bash" > $OUTPUT/bin.sh
+echo "echo 'Hello World from Component No. 2 on device $DEVICE using config tag $CONFIG'" >> $OUTPUT/bin.sh
+chmod +x $OUTPUT/bin.sh
